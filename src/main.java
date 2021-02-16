aa2a427c558cd6ab3ca5c67d1b389387f046cad1
Elvis Miranda <elvismiranda924g@gmail.com>
2021-02-16 15:11:42 -0500

    fix

--- src/main.java
+++ src/main.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
-import javax.swing.text.PasswordView;
-import java.util.HashMap;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class main {
 
@@ -45,57 +43,80 @@ public class main {
     }
 
     public static void main(String[] args) {
+        int passwordInput, option, opt2, password;
+        String userInput, usuarioActual, datos, pedido = "";
 
-        String user = "pedro2001";
-        int password = 2020; // Data Usuario y contraseña
-        int passwordInput, option;
-        String userInput, datos, pedido = "";
+        // datos de usuario y passwords
+        List<Integer> passwords = new ArrayList<Integer>();
+        List<String> users = new ArrayList<String>();
+        passwords.add(2020);
+        users.add("pedro2001");
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese su nombre de Usuario");
-        userInput = sc.nextLine();
-        System.out.println("Ingrese su Contraseña");
-        passwordInput = sc.nextInt();
+        do {
+            System.out.println("\n Bienvenido \n 1. registrarse \n 2. inicio de sesion \n");
+            opt2 = sc.nextInt();
+            sc.nextLine();
 
-        if (userInput.equals(user) && passwordInput == password) {
+            if (opt2 == 1) {
+                System.out.println("Ingrese el nombre de usuario para su cuenta");
+                users.add(sc.nextLine());
+                System.out.println("Ingrese su contraseña para la cuenta");
+                passwords.add(sc.nextInt());
 
-            System.out.println("Bienvenido al Sistema" + " " + user); // caso el usuario ingresa al sistema
+                System.out.println("Cuenta creada con exito!!");
 
-            do {
-                System.out.println(
-                        "\n Menu\n 1. Cambiar la contraseña \n 2. Llenar información \n 3. Pedido \n 4. Salir");
-                option = sc.nextInt();
+            } else if (opt2 == 2) {
+                System.out.println("\n Ingrese su nombre de Usuario");
+                userInput = sc.nextLine();
+                System.out.println("Ingrese su Contraseña");
+                passwordInput = sc.nextInt();
 
-                if (option == 1) {
-                    System.out.println("Ingrese su nueva contraseña");
-                    password = sc.nextInt();
-                    System.out.println("\n Su contraseña a sido actualizada a: " + password + " " + "con exito!!");
-                } else if (option == 2) {
-                    datos = datos();
-                    System.out.println("\n Sus datos son: \n" + datos);
-                } else if (option == 3) {
-                    pedido = pedido();
-                    System.out.println("\n Ha seleccionado: \n" + pedido);
-                } else if (option == 4) {
-                    if (pedido.equals("")) {
-                        System.out.println("Esperamos verte pronto. cierre de sesion a las: " + horaPedido());
-                    } else {
-                        System.out.println("\n Su pedido de: " + pedido + " se realizo con exito a la hora: "
-                                + horaPedido() + " El mismo sera enviado a domicilio, Disfrute su compra :D");
-                    }
+                usuarioActual = userInput;
 
+                if (users.contains(userInput) && passwords.contains(passwordInput)) {
+
+                    System.out.println("Bienvenido al Sistema" + " " + usuarioActual); // caso el usuario ingresa al
+                                                                                       // sistema
+
+                    do {
+                        System.out.println(
+                                "\n Menu\n 1. Cambiar la contraseña \n 2. Llenar información \n 3. Pedido \n 4. Salir");
+                        option = sc.nextInt();
+
+                        if (option == 1) {
+                            System.out.println("Ingrese su nueva contraseña");
+                            password = sc.nextInt();
+                            System.out.println(
+                                    "\n Su contraseña a sido actualizada a: " + password + " " + "con exito!!");
+                        } else if (option == 2) {
+                            datos = datos();
+                            System.out.println("\n Sus datos son: \n" + datos);
+                        } else if (option == 3) {
+                            pedido = pedido();
+                            System.out.println("\n Ha seleccionado: \n" + pedido);
+                        } else if (option == 4) {
+                            if (pedido.equals("")) {
+                                System.out.println("Esperamos verte pronto. cierre de sesion a las: " + horaPedido());
+                            } else {
+                                System.out.println("\n Su pedido de: " + pedido + " se realizo con exito a la hora: "
+                                        + horaPedido() + " El mismo sera enviado a domicilio, Disfrute su compra :D");
+                            }
+
+                        } else {
+                            System.out.println("Ingrese una opcion valida");
+                        }
+                    } while (option != 4);
+
+                } else if (users.contains(userInput)) {
+                    System.out.println("Error, la contraseña es incorrecta!!"); // caso que la contraseña es incorrecta
                 } else {
-                    System.out.println("Ingrese una opcion valida");
+                    System.out.println("Error, el usuario es incorrecto!!"); // caso donde el usuario es incorrecto
                 }
-            } while (option != 4);
 
-        } else if (userInput.equals(user)) {
-            System.out.println("Error, la contraseña es incorrecta!!"); // caso que la contraseña es incorrecta
-        } else {
-            System.out.println("Error, el usuario es incorrecto!!"); // caso donde el usuario es incorrecto
-        }
-
+            }
+        } while (opt2 != 2);
     }
 
 }
