2099dd51d13698e6dec0a1214f5a664eb7936edd
Elvis Miranda <elvismiranda924g@gmail.com>
2021-02-17 18:12:49 -0500

    correcion clase

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class main {
+public class Main {
 
     public static String datos() { // Funcion para la toma de datos
         Scanner sc = new Scanner(System.in);
